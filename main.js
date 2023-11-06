aaf43389b6092b534029429624793c02d02aacff
Ciaran Brown <ciaranbrown@ciarans-air.home>
2023-11-06 16:20:01 +0000

    Navigation links finished using anchor smooth scrolling using JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Smooth Slide-In Animation (Left, Right and Up) */
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     let targetOne = entry.target.classList.contains('hide-animation-up');
@@ -21,3 +23,16 @@ const observer = new IntersectionObserver(entries => {
 
 const hiddenElements = document.querySelectorAll('.hide-animation-up, .hide-animation-left, .hide-animation-right');
 hiddenElements.forEach(el => observer.observe(el));
+
+
+/* Anchor Scroll for Navigation Bar */
+
+document.querySelectorAll('a[href^="#]').forEach(anchor => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
